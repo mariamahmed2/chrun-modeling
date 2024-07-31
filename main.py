e10b1a5ec52003f1e80cae58165b964df598e1d8
mariamahmed2 <es-MariamAhmed2024@alexu.edu.eg>
2024-07-31 03:22:11 +0300

    building fastapi endpoint

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+'''
+@app.post("/")
+def read_root():
+    return {"Hello World"}
+'''
+@app.get("/")
+def read_item():
+    return {"Hello"}
+
+
+# In Terminal "uvicorn mian:app --reload "
