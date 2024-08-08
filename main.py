a64e12db0ca891d5fd3edcb8625e9adbdb6d2cf6
mariamahmed2 <es-MariamAhmed2024@alexu.edu.eg>
2024-08-09 02:16:45 +0300

    compare different ml models metrics

--- main.py
+++ main.py
@@ -1,15 +1,45 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form, HTTPException
+from utils import predict_new
+from pydantic import BaseModel, Field
+from dotenv import load_dotenv
+import os
 
-app = FastAPI()
-'''
-@app.post("/")
-def read_root():
-    return {"Hello World"}
-'''
-@app.get("/")
-def read_item():
-    return {"Hello"}
+# laod .env
+_ = load_dotenv(override=True)
+SECRET_KEY = os.getenv('API_KEY_TOKEN')
+
+app = FastAPI(title='Churn Prediction')
 
 
-# In Terminal "uvicorn mian:app --reload "
-# localhost8000
+@app.get('/', tags=['General'])
+def home():
+    return {'Up & Running'}
+
+
+# Pydantic class
+class CustomerData(BaseModel):
+    CreditScore: float
+    Geography: str
+    Gender: str
+    Age: int
+    Tenure: int
+    Balance: float
+    NumOfProducts: int
+    HasCrCard: int
+    IsActiveMember: int
+    EstimatedSalary: float
+
+
+
+
+@app.post('/predict', tags=['Churn'])
+def churn_prediction(data: CustomerData):
+
+    # if key_token not in [SECRET_KEY]:
+    #     raise HTTPException(status_code=403, detail='You are not authorized to use this API')
+         
+    
+    # call (predict_new) from utils.py
+    case_pred = predict_new(data=data)
+
+    return {f'Prediction is {case_pred}'}
